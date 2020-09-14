2ac55c5a4fcd2d2c9a01752c56699676a03217cc
Viviana Lugo <vivi28lu@gmail.com>
2020-09-14 02:52:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,9 +17,7 @@ def speak_to_grandma (phrase = "I LOVE YOU GRANDMA!")
 if phrase == "I LOVE YOU GRANDMA!"
    "I LOVE YOU TOO PUMPKIN!"
  elsif phrase == phrase.upcase
-   "I LOVE YOU TOO PUMPKIN!"
-elsif speak_to_grandma = "HELLO!"
    "NO, NOT SINCE 1938!"
-   
-
+elsif phrase == phrase.downcase
+   "HUH?! SPEAK UP, SONNY!"
 end
