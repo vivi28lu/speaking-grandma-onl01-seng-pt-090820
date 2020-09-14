2aa11626428677582c27137226604c8e88d6f844
Viviana Lugo <vivi28lu@gmail.com>
2020-09-14 02:51:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,8 @@
 def speak_to_grandma (phrase = "I LOVE YOU GRANDMA!")
 
 if phrase == "I LOVE YOU GRANDMA!"
-   "HUH?! SPEAK UP, SONNY!"
- else 
+   "I LOVE YOU TOO PUMPKIN!"
+ elsif phrase == phrase.upcase
    "I LOVE YOU TOO PUMPKIN!"
 elsif speak_to_grandma = "HELLO!"
    "NO, NOT SINCE 1938!"
