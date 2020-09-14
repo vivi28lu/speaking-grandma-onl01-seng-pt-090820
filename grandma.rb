e4ea67b528ecf108e96b16762b387eebabeabc75
Viviana Lugo <vivi28lu@gmail.com>
2020-09-14 02:45:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,6 @@ if speak_to_grandma.downcase
   puts "HUH?! SPEAK UP, SONNY!"
 elsif speak_to_grandma = "HELLO!"
   puts "NO, NOT SINCE 1938!"
-else speak_to_grandma (phrase="I LOVE YOU GRANDMA!")
+else speak_to_grandma
   puts "I LOVE YOU TOO PUMPKIN!"
 end
